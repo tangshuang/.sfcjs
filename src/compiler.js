8fbf21abe368fa5ed0044ff015e7540601d19c8c
tisontang(唐霜) <tisontang@tencent.com>
2021-08-13 17:45:09 +0800

    feat: finish compile module

--- src/compiler.js
+++ src/compiler.js
@@ -1,8 +1,9 @@
 // import { parse as parseJs } from '@babel/parser'
 import parseCssAst from './css-parser'
 import { parseHtmlToAst, traverseAst as traverseHtmlAst } from 'abs-html'
-import { clear } from './utils'
-import { tokenizer } from './js-parser'
+import { clear, padding, each, camelcase } from './utils'
+import { tokenizer as tokenizeJs } from './js-parser'
+import { prettyCode } from './code-prettier'
 
 function parseJs(sourceCode) {
   const deps = []
@@ -46,7 +47,7 @@ function parseJs(sourceCode) {
     return lines
   }, []).join('\n')
 
-  const tokens = tokenizer(scripts)
+  const tokens = tokenizeJs(scripts)
   const createReactive = (code) => {
     return code.replace(/let(.*?)=([\w\W]+?);$/, (_, name, value) => {
       return `let ${name.trim()} = SFCJS.reactive(${value.trim()}, () => ${name.trim()});\n`
@@ -75,6 +76,11 @@ function parseJs(sourceCode) {
 
         // 结束标记
         if (!localScope.length && next === ';') {
+          code += createReactive(reactive)
+          break
+        }
+        // TODO 需要支持不使用分号结尾的脚本
+        if (!localScope.length && next === '\n') {
           code += createReactive(reactive)
           break
         }
@@ -113,16 +119,180 @@ function parseJs(sourceCode) {
 
 function parseCss(sourceCode, source) {
   const ast = parseCssAst(sourceCode, { source })
-  console.log(ast)
   let code = 'function(r) {\n'
+
+  const { stylesheet = {} } = ast
+  const { rules = [] } = stylesheet
+  const fnsSections = []
+  const sections = []
+  each(rules, (rule) => {
+    if (rule.type === 'fns') {
+      fnsSections.push(rule)
+    }
+    else {
+      sections.push(rule)
+    }
+  })
+
+  const createValue = (value) => {
+    const wrapped = value.replace(/^\('\{\{(.*?)\}\}'\)$/, '[[$1]]').trim()
+    const interpolated = wrapped.replace(/\[\[(.*?)\]\]/g, '${$1}')
+    const real = interpolated === wrapped ? `'${wrapped}'`
+      : /^\$\{.*?\}$/.test(interpolated) ? wrapped.substring(2, wrapped.length - 2) : '`' + interpolated + '`'
+    return real
+  }
+  const createProps = (declarations) => {
+    const properties = []
+    each(declarations, ({ property, value }) => {
+      if (property === 'fns') {
+        const fns = value.split(/,\s*?(?=\w+\()/).map((invoker) => {
+          const [name, paramsStr = ''] = invoker.split(/(?=\()/)
+          const paramsT = paramsStr.trim()
+          const params = paramsT.substring(1, paramsT.length - 1).split(',')
+            .map(item => item.trim())
+            .map(item => createValue(item))
+          return `${name}(${params.join(',')})`
+        })
+        properties.push({ fns })
+        return
+      }
+
+      const propStr = createValue(property)
+      const prop = camelcase(propStr)
+      properties.push({
+        name: prop,
+        value: createValue(value),
+      })
+    })
+    return properties
+  }
+
+  const fnsMapping = {}
+  each(fnsSections, ({ rules }) => {
+    each(rules, (rule) => {
+      const { selectors, declarations } = rule
+      const exp = selectors[0]
+      const [name, params] = exp.split(/(?=\()/)
+      const properties = createProps(declarations)
+      const fn = `const ${name} = ${params} => ({ ${properties.map(({ name, value }) => `${name}:${value}`).join(',')} })`
+      fnsMapping[name] = fn
+    })
+  })
+  const fns = Object.values(fnsMapping)
+
+  const createRule = (section) => {
+    const { selectors, declarations } = section
+    const properties = createProps(declarations)
+
+    let rule = `r('${selectors.join(',')}',`
+
+    const props = []
+    each(properties, (item) => {
+      const { name, value, fns } = item
+      if (fns) {
+        if (props.length) {
+          rule += `{${props.join(',')}},`
+          props.length = 0
+        }
+        rule += fns.join(',')
+      }
+      else {
+        props.push(`${name}: ${value}`)
+      }
+    })
+
+    if (props.length) {
+      rule += `{${props.join(',')}}`
+    }
+
+    rule += ')'
+
+    return rule
+  }
+
+  const css = []
+  let inIf = ''
+  each(sections, (section) => {
+    const { type } = section
+    if (type === 'if') {
+      if (inIf) {
+        throw new Error('@if不允许嵌套')
+      }
+
+      const { condition, rules } = section
+      inIf = createValue(condition) + '?'
+      const rule = rules.map(createRule).join(',')
+      inIf += rules.length > 1 ? `[${rule}]`
+        : rules.length === 1 ? rule
+        : 'null'
+
+      return
+    }
+    if (type === 'elseif') {
+      if (!inIf) {
+        throw new Error('@elseif必须跟在@if后面')
+      }
+
+      const { condition, rules } = section
+      inIf += `:${createValue(condition)}?`
+      const rule = rules.map(createRule).join(',')
+      inIf += rules.length > 1 ? `[${rule}]`
+        : rules.length === 1 ? rule
+        : 'null'
+
+      return
+    }
+    if (type === 'else') {
+      if (!inIf) {
+        throw new Error('@else必须跟在@if后面')
+      }
+
+      const { rules } = section
+      const rule = rules.map(createRule).join(',')
+      inIf += ':'
+      inIf += rules.length > 1 ? `[${rule}]`
+        : rules.length === 1 ? rule
+        : 'null'
+
+      css.push(inIf)
+      inIf = ''
+
+      return
+    }
+
+    // 直接结束if
+    if (inIf) {
+      inIf += ':null'
+      css.push(inIf)
+      inIf = ''
+    }
+
+    if (type === 'for') {
+      const { items, item, index, rules } = section
+      const rule = `...${items}.map((${item},${index}) => [${rules.map(createRule).join(',')}])`
+      css.push(rule)
+      return
+    }
+
+    if (type === 'rule') {
+      const rule = createRule(section)
+      css.push(rule)
+      return
+    }
+  })
+
+  code += fns.join(';\n') + ';\n'
+
+  code += `return [\n${css.join(',\n')}\n];\n`
+
   code += '}'
   return code
 }
 
-function parseHtml(sourceCode, source) {
+function parseHtml(sourceCode) {
   const htmlAst = parseHtmlToAst(sourceCode.trim())
 
-  let code = 'function(h) {\n'
+  let code = 'function(h) {\nreturn '
   traverseHtmlAst(htmlAst, {
     '*': {
       enter(node, parent) {
@@ -134,8 +304,90 @@ function parseHtml(sourceCode, source) {
       },
     },
   })
-  console.log(htmlAst)
-  code += '}'
+
+  const interpolate = (str) => {
+    const res = str.replace(/{{(.*?)}}/g, '${$1}')
+    if (str === res) {
+      return "'" + str + "'"
+    }
+    return '`' + res + '`'
+  }
+
+  const create = (obj) => {
+    const attrs = []
+    const props = []
+    const events = []
+    each(obj, (value, key) => {
+      const v = value && typeof value === 'object' ? create(value)
+        : typeof vlaue === 'string' ? interpolate(value)
+        : value
+      if (key.indexOf(':') === 0) {
+        const realKey = key.substr(1)
+        const k = camelcase(realKey)
+        props.push([k, v])
+      }
+      else if (key.indexOf('@') === 0) {
+        const k = key.substr(1)
+        events.push([k, `event => ${v}`])
+      }
+      else {
+        attrs.push([key, `'${v}'`])
+      }
+    })
+
+    const inner = [
+      ['props', props],
+      ['attrs', attrs],
+      ['events', events],
+    ].map((item) => {
+      const [name, info] = item
+      if (!info.length) {
+        return
+      }
+      let res = name + ': {'
+      res += info.map(([key, value]) => `${key}: ${value}`).join(',')
+      res += '}'
+      return res
+    }).filter(item => !!item).join(',')
+
+    if (!inner) {
+      return ''
+    }
+
+    return `{${inner}}`
+  }
+
+  const build = (astNode) => {
+    const [type, props, ...children] = astNode
+
+    let attrs = ''
+    let subs = []
+
+    if (props) {
+      attrs = create(props)
+    }
+
+    if (children.length && children.some(item => !!item)) {
+      each(children, (child) => {
+        if (typeof child === 'string') {
+          const node = interpolate(child)
+          subs.push(node)
+        }
+        else {
+          const node = build(child)
+          subs.push(node)
+        }
+      })
+    }
+
+    const inner = subs.join(',\n')
+    const params = [`'${type}'`, attrs, inner].filter(item => !!item)
+    const code = `h(${params.join(',\n')})`
+    return code
+  }
+
+  code += build(htmlAst)
+  code += ';\n}'
 
   return code
 }
@@ -174,17 +426,20 @@ export function genComponent({ imports, deps, components, jsCode, cssCode, htmlC
     `SFCJS.define("${source}", [${deps.map(([, src]) => `"${src}"`).join(', ')}], function(${deps.map(([name]) => `${name}`).join(', ')}) {`,
     jsCode,
     `  const components = {\n${components.map(([name]) => `    ${name}`).join(',\n')}\n  }`,
-    `  return {\n    components,\n    css: ${cssCode},\n    dom: ${htmlCode}\n  }`,
+    `  return {\n    components,\n    style: ${cssCode},\n    render: ${htmlCode}\n  }`,
     '});',
   ].join('\n')
   return output
 }
 
 export function compileComponent(text, source) {
+  const start = performance.now()
   const asts = parseComponent(text, source)
   const code = genComponent(asts, source)
-  console.log(code)
-  return code
+  const res = prettyCode(code)
+  const end = performance.now()
+  console.log(res, 'cost time:', end - start)
+  return res
 }
 
 export function loadComponent(src) {
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,30 @@
 export function each(obj, fn) {
+  if (Array.isArray(obj)) {
+    for (let i = 0, len = obj.length; i < len; i ++) {
+      const item = obj[i]
+      fn(item, i)
+    }
+    return
+  }
+
   const keys = Object.keys(obj)
   for (let i = 0, len = keys.length; i < len; i ++) {
     const key = keys[i]
     const value = obj[key]
-    fn(key, value)
+    fn(value, key)
   }
 }
 
 export function clear(str) {
   return str.replace(/\/\*.*?\*\//gmi, '').replace(/\/\/.*?[\n$]/, '')
+}
+
+export function padding(count) {
+  return new Array(count).fill(' ').join('')
+}
+
+export function camelcase(str) {
+  return str.replace(/[-_]\w/ig, (matched, index) => {
+    return matched[1].toUpperCase()
+  }).replace(/\s+/g, '')
 }
